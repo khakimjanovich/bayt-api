8c47fa8625d9cb4a038ca842f32c92d847065794
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-09-16 15:39:17 +0500

    fix: bomdod can be nullable

--- src/Data/MosqueResource.php
+++ src/Data/MosqueResource.php
@@ -15,7 +15,7 @@ class MosqueResource extends Data
         public int $province_id,
         public string $name,
         public string $url,
-        public string $bomodod,
+        public ?string $bomodod,
         public string $xufton,
         #[MapName('hasLocation')]
         public bool $has_location,
